495239c08040c6dcd210df660a47f475a8a8f752
Vasyl Vlasiuk <v.vlasiuk@brainence.com>
2024-07-25 14:08:55 +0300

    test 2

--- testProcessedButton.js
+++ testProcessedButton.js
@@ -6,9 +6,9 @@ define(function (require) {
         const config = { childList: true, subtree: true };
 
         function searchTree(element, matchingTitle) {
-            if (element && element.querySelectorAll(".status-container") && element.baseURI.indexOf("ProcessedOrders") > - 1) {
-                console.log("Founded .status-container");
-                return element.querySelectorAll("div")[0];
+            if (element && element.querySelectorAll("div[ng-controller='ProcessedOrdersModule'] .status-container") && element.baseURI.indexOf("ProcessedOrders") > - 1) {
+                console.log("Founded div[ng-controller='ProcessedOrdersModule'] .status-container");
+                return element.querySelectorAll("div[ng-controller='ProcessedOrdersModule'] .status-container")[0];
             }
             else if (element && element.children != null) {
                 let i;
@@ -25,9 +25,9 @@ define(function (require) {
             for (const mutation of mutationsList) {
                 if (mutation.type === "childList") {
                     for (const node of mutation.addedNodes) {
-                        let result = searchTree(node, "external-ui-component");
+                        let result = searchTree(node, "div[ng-controller='ProcessedOrdersModule'] .status-container");
                         if (result) {
-                            console.log("Founded needed IFrame");
+                            console.log("Founded needed div[ng-controller='ProcessedOrdersModule'] .status-container");
                             console.log(result);
                             // result.src = result.src + "&email=" + session.email;
                             return;
