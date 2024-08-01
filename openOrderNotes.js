7b24302bbbe26119228250e05abdbaed032cbf4f
Vasyl Vlasiuk <v.vlasiuk@brainence.com>
2024-08-01 11:26:24 +0300

    styles added test

--- openOrderNotes.js
+++ openOrderNotes.js
@@ -146,6 +146,7 @@ define(function(require) {
 
     const setupStyles = function(isOld){
         if (!window.stylesAdded) {
+            console.log('setting up styles');
             const stylesToAdd = isOld ? stylesOld : styles;
             const styleElem = document.createElement('style');
             styleElem.innerHTML = stylesToAdd;
