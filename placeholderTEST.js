314a8ba7032fca242140e6e99b338b0bb136c5a4
Vasyl Vlasiuk <v.vlasiuk@brainence.com>
2025-05-21 14:52:31 +0300

    c

--- placeholderTEST.js
+++ placeholderTEST.js
@@ -2,89 +2,48 @@
 
 define(function(require) {
   const placeholderManager = require("core/placeholderManager");
-  const macroService = new Services.MacroService();
   const orderService = new Services.OrderService();
-  const rulesService = new Services.RulesEngineService();
 
   const placeholder = function ($scope) {
     const vm = this;
     vm.scope = $scope;
-    vm.rules = [];
+    vm.buttons = [];
 
     vm.isEnabled = (itemKey) => {
-      const ruleId = Number(itemKey.substring(9));
-      const rule = vm.rules.find(r => r.pkRuleId === ruleId);
-      return !!rule;
+      return true;
     };
 
     vm.getItems = () => {
-      return [
-        {
-          key: "123132123123",
-          text: "click me",
-          icon: "fa func fa-cogs"
-        }
-      ];
-      return vm.rules.map(i => {
-        return {
-          key: `run-rule-${i.pkRuleId}`,
-          text: `Run rule: ${i.RuleName}`,
-          icon: "fa func fa-cogs",
-        };
-      })
+      return vm.buttons;
     };
 
     vm.onClick = (itemKey, $event) => {
-      const ruleId = Number(itemKey.substring(9));
-      const chunkSize = 100;
-
-      for (let i = 0; i < $scope.viewStats.selected_orders.length; i += chunkSize) {
-        const chunk = $scope.viewStats.selected_orders.slice(i, i + chunkSize);
-
-        orderService.runRulesEngine(chunk, ruleId, (response) => {
-          console.log(response);
-        });
-      }
+      console.log("click " + itemKey);
     };
 
-    // angular.element(document).ready(() => {
-    //   vm.scope.$watch(() => vm.scope.viewStats.view_configuration.ViewId, function (newVal, oldVal) {
-    //     if (!!newVal && newVal !== oldVal){
-    //       vm.reloadRules(vm.scope.viewStats.view_configuration.ViewName);
-    //     }
-    //   });
-    // });
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
     angular.element(document).ready(function () {
       console.log("asd");
-      vm.ordersSelectedWatch = $scope.$watch(() => $scope.viewStats.selected_orders, function(newVal, oldVal){
-          console.log("asd");
-      }, true);
-    });
-
-    vm.reloadRules = (viewName) => {
-      macroService.GetMacroConfigurations((response) => {
-        const macroConfigs = response.result.filter((x) => x.ApplicationName === "Pluggable_test" && x.MacroName === "Pluggable_test");
-        
-        const viewConfigs = macroConfigs.filter(c => {
-          const viewParameter = c.Parameters.find(p => p.ParameterName === "view");
-          return viewParameter?.ParameterValue === viewName;
-        });
-
-        if (!viewConfigs.length) {
-          vm.rules = [];
+      vm.viewChanged = $scope.$watch(() => $scope.viewStats.viewConfiguration.ViewId, function(newVal, oldVal){
+        if(oldVal === newVal){
           return;
         }
-
-        const rules = vm.scope.userConfiguration.rules.rules;
-
-        vm.rules = viewConfigs.map(c => {
-          const ruleParameter = c.Parameters.find(p => p.ParameterName === "rule");
-          const rule = rules.find(r => r.RuleName === ruleParameter?.ParameterValue);
-          return rule;
-        }).filter(r => !!r);
-      });
-    };
+          const count = getRandomInt(1, 5);
+          vm.buttons = [];
+          for(let i = 0; i < count; i++){
+            vm.buttons.push({
+              name: "btn_" + i,
+              key: "someBtn" + i,
+              text: "button " + i
+            });
+          }
+      }, true);
+    });
   };
   
   placeholderManager.register("OpenOrders_OrderControlButtons", placeholder);
